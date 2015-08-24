89b3c93a15632abb908583917e31c8c69fef518f
Andy <php360@qq.com>
2015-08-24 14:03:17 +0800

    删除测试文件

--- src/Route/Route.php
+++
@@ -1,6 +0,0 @@
-<?php namespace Agile\Route;
-
-
-class Route {
-
-}
--- src/test.php
+++
@@ -1,8 +0,0 @@
-<?php namespace Agile;
-
-class Test {
-
-    public static function test() {
-        echo $_SERVER['REQUEST_TIME'];
-    }
-}
